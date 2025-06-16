20e5ec2f7ae60f5890f88735f2e4f1c3fded4a46
Raulif <raul4cade@gmail.com>
2025-06-16 15:03:05 +0200

    Empty list

--- src/components/NamesList/NamesList.svelte
+++ src/components/NamesList/NamesList.svelte
@@ -21,20 +21,20 @@
 		</div>
 	{:else if error}
 		Die Namensliste konnten nicht geladen werden
-	{:else if !names.length}
-		<div class="fixed top-[25%] left-0 flex w-full items-center justify-center px-4">
-			<p
-				class="raleway-bold rounded-xl border-3 border-white bg-violet-200 px-10 py-8 text-center text-2xl shadow-2xl"
-			>
-				Die Namensliste ist noch leer
-			</p>
-		</div>
 	{:else}
 		<ul class="flex h-full w-full flex-col-reverse justify-end py-2">
 			{#each names as name (name.name)}
 				<li animate:flip={{ duration: 250 }} in:fade out:fade>
 					<ListItem {...name} />
 				</li>
+			{:else}
+				<div class="fixed top-[25%] left-0 flex w-full">
+					<p
+						class=" w-full gap-2 bg-white px-4 py-4 pb-4 text-center poppins-bold flex items-center justify-center text-xl"
+					>
+						Die Namensliste ist noch leer
+					</p>
+				</div>
 			{/each}
 		</ul>
 	{/if}
