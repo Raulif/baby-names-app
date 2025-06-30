17c9875276feaeda8b2a6f8aa89ab5f37a2de700
Raulif <raul4cade@gmail.com>
2025-06-30 17:37:52 +0200

    Enable auto remove from toast

--- src/store/toastStore.ts
+++ src/store/toastStore.ts
@@ -8,7 +8,7 @@ export const addToast = (toast: Partial<Toast>) => {
   // Push the toast to the top of the list of toasts
   toasts.update((all) => [newToast, ...all]);
   // If toast hast timeout, dismiss it after "timeout" amount of time.
-  if (!newToast.timeout) setTimeout(() => dismissToast(newToast._id), newToast.timeout);
+  if (newToast.timeout) setTimeout(() => dismissToast(newToast._id), newToast.timeout);
 };
 
 export const dismissToast = (id: number) => {
