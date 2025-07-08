a7433c2e59817ae7b9b24c7f51b52ad00a5488c4
Raulif <raul4cade@gmail.com>
2025-07-08 14:27:09 +0200

    Add notification click

--- src/service-worker.js
+++ src/service-worker.js
@@ -31,30 +31,59 @@ async function isClientFocused() {
 	return clientIsFocused;
 }
 
+async function getMatchingClient(url) {
+	const urlToOpen = new URL(url).href;
+	const clients = await getClients();
+	let matchingClient;
+	clients.forEach((c) => {
+		if (matchingClient) return;
+		if (client.url === urlToOpen) {
+			matchingClient = client;
+		}
+	});
+	return matchingClient;
+}
+
 const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
 
 sw.addEventListener('activate', (e) => {
 	console.log('[SERVICE WORKER] ACTIVATED: ', e);
 });
 
-self.addEventListener('push', async (event) => {
-	const data = JSON.parse(event.data.text());
-	const isFocused = await isClientFocused();
-	if (isFocused) {
-		// Send message to focused window
-		const clients = await getClients();
-		clients.forEach((client) => {
-			client.postMessage(data);
-		});
-	} else {
-		const options = {
-			body: data.notification.text,
-			icon: '/icon.png',
-			badge: '/icon.png'
-		};
-		// Send push notification to inactive app
-		self.registration.showNotification('Baby Names App', options);
+self.addEventListener('push', (event) => {
+	async function onPush() {
+		const data = JSON.parse(event.data.text());
+		const isFocused = await isClientFocused();
+		if (isFocused) {
+			// Send message to focused window
+			const clients = await getClients();
+			clients.forEach((client) => {
+				client.postMessage(data);
+			});
+		} else {
+			const options = {
+				body: data.notification.text,
+				icon: '/icon.png',
+				badge: '/icon.png'
+			};
+			// Send push notification to inactive app
+			self.registration.showNotification('Baby Names App', options);
+		}
+		const parent = data.notification.issuer === 'mama' ? 'papa' : 'mama';
+		await updateNotificationConsumption(data.notification._id, parent);
 	}
-	const parent = data.notification.issuer === 'mama' ? 'papa' : 'mama';
-	await updateNotificationConsumption(data.notification._id, parent);
+	event.waitUntil(onPush());
+});
+
+self.addEventListener('notificationclick', (e) => {
+	async function onClick() {
+		const matchingClient = await getMatchingClient(self.location.origin);
+		if (matchingClient) {
+			matchingClient.focus();
+		} else {
+			clients.openWindow(new URL(self.location.origin).href);
+		}
+		return notification.close();
+	}
+	e.waitUntil(onClick());
 });
